1fb341b02aca7829a140b626bfe07ca1b7c12fd6
Andrey Gridyaev <agridyaev@gmail.com>
2021-10-10 14:06:21 +0300

    tests/test_ui: add wait for particular text in page title

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,14 +1,23 @@
 from time import sleep
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_google(driver):
     driver.get("https://google.ru")
     driver.check_in_title("Google")
-    input = driver.find_element_by_css_selector("input[type='text']")
-    input.send_keys("Selenoid")
-    input.send_keys(Keys.RETURN)
-    driver.check_in_title("Selenoid")
+    search_input = driver.find_element_by_css_selector(
+        "input[type='text']")
+
+    text = "Selenoid"
+    search_input.send_keys(text)
+    search_input.send_keys(Keys.RETURN)
+
+    WebDriverWait(driver, 10).until(
+        EC.title_contains(text)
+    )
+    driver.check_in_title(text)
 
 
 def test_yandex(driver):
