86be09725a28f178131948e97d62ae319575b17d
Andrey Gridyaev <agridyaev@gmail.com>
2023-06-30 23:28:05 +0300

    tests/test_ui: update selectors

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 def test_google(driver):
     driver.get("https://google.ru")
     driver.check_in_title("Google")
-    input = driver.find_element_by_css_selector("input[type='text']")
+    input = driver.find_element_by_css_selector("textarea")
     input.send_keys("Selenoid")
     input.send_keys(Keys.RETURN)
     driver.check_in_title("Selenoid")
@@ -13,5 +13,5 @@ def test_google(driver):
 
 def test_yandex(driver):
     driver.get("https://ya.ru")
-    driver.check_element_presence("#text")
+    driver.check_element_presence("input[id='text']")
     driver.check_in_title("Яндекс")
